739b59a8b0abd2b3212edec98555435fc48994a6
Holger Nahrstaedt <holgernahrstaedt@gmx.de>
2017-07-04 21:25:11 +0200

    fix

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -e
+
+get_platform () {
+  case "$OSTYPE" in
+    darwin*) echo "darwin" ;;
+    linux*)  echo "linux" ;;
+    *)       echo "Unsupported platform" >&2; exit -1 ;;
+  esac
+}
 
 
 TARGET_DIR=$PWD/texlive
@@ -12,37 +20,25 @@ cat > $PROFILE <<END_CAT
 # texlive.profile generated on 2017-07-04
 selected_scheme scheme-custom
 TEXDIR $TARGET_DIR
-TEXMFCONFIG $TEXMFSYSCONFIG
-TEXMFHOME $TEXMFLOCAL
+TEXMFHOME $TARGET_DIR/texmf
 TEXMFLOCAL $TARGET_DIR/texmf-local
+TEXMFCONFIG $TARGET_DIR/texmf-config
 TEXMFSYSCONFIG $TARGET_DIR/texmf-config
+TEXMFVAR $TARGET_DIR/texmf-var
 TEXMFSYSVAR $TARGET_DIR/texmf-var
-TEXMFVAR $TEXMFSYSVAR
+binary_x86_64-darwin 1
 binary_x86_64-linux 1
 collection-basic 1
-collection-bibtexextra 1
+collection-binextra 1
 collection-latex 1
-collection-latexrecommended 1
-collection-latexextra 1
-collection-luatex 1
-collection-xetex 1
-collection-langgerman 1
-collection-humanities 1
-collection-fontsrecommended 1
-collection-fontsextra 1
-collection-fontutils 1
-collection-publishers 1
-collection-pictures 1
-portable 1
 option_adjustrepo 1
-option_autobackup 1
-option_backupdir tlpkg/backups
-option_desktop_integration 
+option_autobackup 0
+option_desktop_integration 0
 option_doc 0
-option_file_assocs 
+option_file_assocs 0
 option_fmt 1
 option_letter 0
-option_menu_integration 
+option_menu_integration 0
 option_path 0
 option_post_code 1
 option_src 0
@@ -51,16 +47,20 @@ option_sys_info /usr/local/share/info
 option_sys_man /usr/local/share/man
 option_w32_multi_user 0
 option_write18_restricted 1
+portable 1
 END_CAT
 
 rm -f installation.profile
 install-tl-*/install-tl -profile $PROFILE
 
+# Install individual packages...
+PATH="$TARGET_DIR/bin/x86_64-$(get_platform):$PATH"
+tlmgr install datatool etoolbox feynmf fp framed glossaries index metapost mfirstuc nomencl substr supertabular textcase xfor xkeyval
+
+
 rm -rf $TARGET_DIR/texmf-dist/doc
 rm -rf $TARGET_DIR/texmf-dist/source
-rm -f texlive.7z
 rm -f texlive.tar.xz
 
 echo "Creating tarball..."
-#7z a texlive.7z texlive >/dev/null
 XZ_OPT=-9 tar cJf texlive.tar.xz texlive
