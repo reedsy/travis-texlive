1f3e2c8d308e76525a0aabe944dc1529117d9a90
Holger Nahrstaedt <holgernahrstaedt@gmx.de>
2017-07-04 19:33:25 +0200

    update

--- prepare.sh
+++ prepare.sh
@@ -2,58 +2,73 @@
 
 set -e
 
+get_platform () {
+  case "$OSTYPE" in
+    darwin*) echo "darwin" ;;
+    linux*)  echo "linux" ;;
+    *)       echo "Unsupported platform" >&2; exit -1 ;;
+  esac
+}
+
 TARGET_DIR=$PWD/texlive
 rm -rf $TARGET_DIR
 
 PROFILE=texlive.profile
+DATE=`date -ju`
 
 cat > $PROFILE <<END_CAT
-# texlive.profile written on Mon Mar 31 09:11:02 2014 UTC
-# It will NOT be updated and reflects only the
-# installation profile at installation time.
+# texlive.profile generated on $DATE
 selected_scheme scheme-custom
 TEXDIR $TARGET_DIR
-TEXMFCONFIG $TEXMFSYSCONFIG
-TEXMFHOME $TEXMFLOCAL
+TEXMFHOME $TARGET_DIR/texmf
 TEXMFLOCAL $TARGET_DIR/texmf-local
+TEXMFCONFIG $TARGET_DIR/texmf-config
 TEXMFSYSCONFIG $TARGET_DIR/texmf-config
+TEXMFVAR $TARGET_DIR/texmf-var
 TEXMFSYSVAR $TARGET_DIR/texmf-var
-TEXMFVAR $TEXMFSYSVAR
+binary_x86_64-darwin 1
 binary_x86_64-linux 1
 collection-basic 1
+collection-bibtexextra 1
 collection-latex 1
 collection-latexrecommended 1
 collection-latexextra 1
 collection-luatex 1
 collection-xetex 1
-collection-langfrench 1
+collection-langgerman 1
 collection-humanities 1
 collection-fontsrecommended 1
 collection-fontsextra 1
 collection-fontutils 1
 collection-publishers 1
-portable 1
+collection-science 1
+collection-pictures 1
 option_adjustrepo 1
-option_autobackup 1
-option_backupdir tlpkg/backups
-option_desktop_integration 
-option_doc 1
-option_file_assocs 
+option_autobackup 0
+option_desktop_integration 0
+option_doc 0
+option_file_assocs 0
 option_fmt 1
 option_letter 0
-option_menu_integration 
+option_menu_integration 0
 option_path 0
 option_post_code 1
-option_src 1
+option_src 0
 option_sys_bin /usr/local/bin
 option_sys_info /usr/local/share/info
 option_sys_man /usr/local/share/man
 option_w32_multi_user 0
 option_write18_restricted 1
+portable 1
 END_CAT
 
 rm -f installation.profile
 install-tl-*/install-tl -profile $PROFILE
+
+# Install individual packages...
+PATH="$TARGET_DIR/bin/x86_64-$(get_platform):$PATH"
+tlmgr install glossaries biber
+
 
 rm -rf $TARGET_DIR/texmf-dist/doc
 rm -rf $TARGET_DIR/texmf-dist/source
